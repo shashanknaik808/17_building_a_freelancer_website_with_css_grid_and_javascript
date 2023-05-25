20680922552dc08167d8e6829af69d1c62ada1c9
Shashank Naik <shashanknaik808@gmail.com>
2023-05-26 00:39:30 +0530

    Displaying the Portfolio

--- js/app.js
+++ js/app.js
@@ -3,11 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function loadPortfolio() {
-    const url = 'data.json';
-
-    fetch(url)
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data.portfolio);
+            let html = ''
+            data.portfolio.forEach(item => {
+                // Build the Template
+                html += `   
+                <div class="item">
+                    <img src="img/${item.id}.jpg">
+                    <div class="content">
+                        <h3>${item.projectname}</h3>
+                        <p>${item.desc}</p>
+                    </div>
+                </div>
+            `;
+            });
+            // after we build the template, we add this into the HTML
+            document.querySelector('#portfolio').innerHTML = html;
         })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
